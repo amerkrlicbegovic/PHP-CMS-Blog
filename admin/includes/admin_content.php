07a520ab591878589118b482ba7cf75044596f05
Amer <amer.krlicbegovic@gmail.com>
2018-09-17 16:45:36 +0200

    <?php

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,9 +7,13 @@
             Blank Page
             <small>Subheading</small>
         </h1>
+        <?php
+        if($database->connection){
+            echo "True";
+        }?>
         <ol class="breadcrumb">
             <li>
-                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
             </li>
             <li class="active">
                 <i class="fa fa-file"></i> Blank Page
---
+++ admin/includes/database.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("new_config.php");
+
+class Database {
+
+
+    public $connection;
+
+    function __construct(){
+
+        $this->open_db_connection();
+
+    }
+
+    /**
+     *
+     */
+    public function open_db_connection(){
+
+
+        // $this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+
+        if($this->connection->connect_errno) {
+
+            die("Database connection failed badly" . $this->connection->connect_error);
+
+        }
+
+    }
+    public function query($sql) {
+    $result = mysqli_query($this->connection,$sql);
+    if(!$result) {
+        die("Query Failed");
+    }
+    return $result;
+
+}
+
+}  // End of Class Database
+
+$database = new Database();
+
